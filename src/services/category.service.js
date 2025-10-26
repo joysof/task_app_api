fb165b3b6ad956307f0bd426233b7e6eaa23b97f
sajib hossen <sajibhossen560@gmail.com>
2025-10-26 16:46:45 +0600

    create category service

---
+++ src/services/category.service.js
@@ -0,0 +1,19 @@
+const httpStatus = require("http-status")
+const ApiError = require("../utils/ApiError")
+const logger = require('../config/logger')
+const {Category} = require("../models")
+
+const createCategory = async(data) =>{
+    if (!data) {
+        throw new ApiError(httpStatus.BAD_REQUEST ,"Name is required")
+    }
+    try {
+        const category = await Category.create(data)
+        return category
+    } catch (error) {
+        logger.error("Error creating category : " ,error)
+        throw new ApiError(httpStatus.BAD_REQUEST , error.message)
+    }
+}
+
+
