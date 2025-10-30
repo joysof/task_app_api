2b24b43c281356ebf249e5d29d92f7bfd60253a2
sajib hossen <sajibhossen560@gmail.com>
2025-10-30 19:36:07 +0600

    all service show by category and subCatagory

--- src/services/service.service.js
+++ src/services/service.service.js
@@ -41,13 +41,56 @@ const getAllService = async (filter , option) =>{
     }
 
   try {
-    const service = await Service.paginate(query , {
+     const populate = "subCategoryId categoryId";
+    const services = await Service.paginate(query , {
       ...option,
-     populate: 'categoryId subCategoryId',
+     populate
     })
-     
+       await Service.populate(services.results, {
+      path: "subCategoryId",
+      select: "name categoryId",
+      populate: { path: "categoryId", model: "Category", select: "name" },
+    });
+    const grouped = {};
+    services.results.forEach(service => {
+      const subCat = service.subCategoryId;
+      const category = subCat?.categoryId;
+
+      if (!category || !subCat) return;
+
+      if (!grouped[category.id]) {
+        grouped[category.id] = {
+          category: {
+            id: category.id.toString(),
+            name: category.name,
+          },
+          subCategories: {},
+        };
+      }
+
+      if (!grouped[category.id].subCategories[subCat.id]) {
+        grouped[category.id].subCategories[subCat.id] = {
+          id: subCat.id,
+          name: subCat.name,
+          services: [],
+        };
+      }
+
+      grouped[category.id].subCategories[subCat.id].services.push({
+        id: service.id,
+        name: service.name,
+        basePrice: service.basePrice,
+        baseQuantity: service.baseQuantity,
+      });
+    });
+
+    
+    const formatted = Object.values(grouped).map(cat => ({
+      category: cat.category,
+      subCategories: Object.values(cat.subCategories),
+    }));
    
-    return service
+    return formatted
   } catch (error) {
     logger.error('Error fetching service:', error);
     throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, error.message);
